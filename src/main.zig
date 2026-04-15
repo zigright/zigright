600bcacadd04f1ad667071abfd6834af0e462fae
Deebakkarthi C R <dbk@deebakkarthi.com>
2026-04-15 13:27:39 -0400

    feat: upgrade to 0.16.0

--- src/main.zig
+++ src/main.zig
@@ -2,21 +2,14 @@ const std = @import("std");
 
 var progname: []const u8 = undefined;
 
-// Unbuffered IO. Sorry Andrew. I won't flush
-var stdout_writer = std.fs.File.stdout().writer(&.{});
-var stderr_writer = std.fs.File.stderr().writer(&.{});
-const stdout = &stdout_writer.interface;
-const stderr = &stderr_writer.interface;
-
-const helpOptions = struct { output_to_stderr: bool = false };
-fn help(opts: helpOptions) !void {
-    var writer: *std.Io.Writer = if (opts.output_to_stderr) stderr else stdout;
+fn help(writer: *std.Io.Writer) !void {
     try writer.print(
         \\Usage: {s} [FILE|-h]
         \\ FILE    a .zig file. If not given, STDIN is used
         \\ -h      print this help message
         \\
     , .{progname});
+    try writer.flush();
 }
 
 /// Helper function that figures out if a path is relative/absolute
@@ -25,53 +18,69 @@ fn help(opts: helpOptions) !void {
 /// std.fs.Dir.openFile() works on both absolute and relative paths but it requires
 /// a handle to the parent dir first
 /// std.fs.path.openFileAbsolute(), as the name suggests works only on absolute paths
-fn openFile(path: []const u8) !std.fs.File {
-    var ret: std.fs.File = undefined;
+fn openFile(io: std.Io, path: []const u8) !std.Io.File {
+    var ret: std.Io.File = undefined;
     if (std.fs.path.isAbsolute(path)) {
-        ret = try std.fs.openFileAbsolute(path, .{ .mode = .read_only });
+        ret = try std.Io.Dir.openFileAbsolute(io, path, .{ .mode = .read_only });
     } else {
-        ret = try std.fs.cwd().openFile(path, .{ .mode = .read_only });
+        ret = try std.Io.Dir.cwd().openFile(io, path, .{ .mode = .read_only });
     }
     return ret;
 }
 
-pub fn main() !void {
-    const gpa = std.heap.page_allocator;
-    const args = try std.process.argsAlloc(gpa);
-    defer std.process.argsFree(gpa, args);
+pub fn main(init: std.process.Init) !void {
+    const gpa = init.gpa;
+    const io = init.io;
+    const args = try init.minimal.args.toSlice(gpa);
+    defer gpa.free(args);
+
+    var stdout_buffer: [1024]u8 = undefined;
+    var stderr_buffer: [1024]u8 = undefined;
+    var stdout_writer = std.Io.File.stdout().writer(io, &stdout_buffer);
+    var stderr_writer = std.Io.File.stderr().writer(io, &stderr_buffer);
+    const stdout = &stdout_writer.interface;
+    const stderr = &stderr_writer.interface;
 
     progname = std.fs.path.basename(args[0]);
 
-    var src_file_handle: std.fs.File = undefined;
-    defer src_file_handle.close();
+    var src_file_handle: std.Io.File = undefined;
+    defer src_file_handle.close(io);
 
     switch (args.len) {
         1 => {
-            src_file_handle = std.fs.File.stdin();
+            src_file_handle = std.Io.File.stdin();
         },
         2 => {
-            if (std.mem.eql(u8, args[1], "-h")) {
-                try help(.{});
-                std.process.exit(0);
+            if (std.mem.startsWith(u8, args[1], "-")) {
+                if (std.mem.eql(u8, args[1], "-h")) {
+                    try help(stdout);
+                    std.process.exit(0);
+                } else {
+                    try help(stderr);
+                    std.process.exit(1);
+                }
             } else {
-                src_file_handle = openFile(args[1]) catch {
-                    try stderr.print("{s}: Couldn't open {s}\n", .{ progname, args[1] });
+                src_file_handle = openFile(io, args[1]) catch {
+                    try stderr.print("{s}: Cannot open {s}\n", .{ progname, args[1] });
+                    try stderr.flush();
                     std.process.exit(1);
                 };
             }
         },
         else => {
-            try help(.{ .output_to_stderr = true });
+            try help(stderr);
             std.process.exit(1);
         },
     }
 
-    var reader = src_file_handle.reader(&.{});
+    var reader = src_file_handle.reader(io, &.{});
     var writer = std.Io.Writer.Allocating.init(gpa);
 
     _ = try reader.interface.streamRemaining(&writer.writer);
 
     const src_code = try writer.toOwnedSliceSentinel(0);
+    defer gpa.free(src_code);
+
     writer.deinit();
 
     // This has to be a var as .deinit() mutates the fields.
@@ -81,6 +90,7 @@ pub fn main() !void {
     for (ast.nodes.items(.tag)) |value| {
         try stdout.print("{}\n", .{value});
     }
+    try stdout.flush();
 
     std.process.exit(0);
 }
