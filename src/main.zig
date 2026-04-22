c5b2758016a2307f849a40c8b63818fe08f9918f
LindirQuenya <53021080+LindirQuenya@users.noreply.github.com>
2026-04-22 11:11:42 -0400

    does it matter

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 const version_data = @import("version_data");
 const assert = std.debug.assert;
-const alloc = std.mem.Allocator;
+const Allocator = std.mem.Allocator;
 
 var progname: []const u8 = undefined;
 
@@ -215,4 +215,29 @@ pub fn main(init: std.process.Init) !void {
     try stdout.flush();
 
     std.process.exit(0);
+}
+
+pub fn branched_alloc(gpa1: std.mem.Allocator, gpa2: std.mem.Allocator, a: bool) !void {
+    var arr: [5]u8 = undefined;
+    if (a) {
+        arr = gpa1.alloc(u8, 5);
+    } else {
+        arr = gpa2.alloc(u8, 5);
+    }
+    // State here?
+}
+
+pub fn branched_alloc2(gpa1: std.mem.Allocator, gpa2: std.mem.Allocator, a: bool) !void {
+    var arr: [5]u8 = undefined;
+    if (a) {
+        arr = gpa1.alloc(u8, 5);
+    }
+    if (!a) {
+        arr = gpa2.alloc(u8, 5);
+    }
+}
+
+pub fn something_and_free(thing: []u8, alloc: std.mem.Allocator) void {
+    defer alloc.free(thing);
+    // And do something interesting, maybe.
 }
