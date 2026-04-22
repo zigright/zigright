628fcebca388e7dd410ab05123a6081db55d980b
Deebakkarthi C R <dbk@deebakkarthi.com>
2026-04-22 05:14:47 -0400

    do we even a commit message at this point

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,15 @@
 const std = @import("std");
+const version_data = @import("version_data");
+const assert = std.debug.assert;
+const alloc = std.mem.Allocator;
 
 var progname: []const u8 = undefined;
+
+fn foo(gpa1: std.mem.Allocator, gpa2: std.mem.Allocator) void {
+    _ = gpa1;
+    _ = gpa2;
+    return;
+}
 
 fn help(writer: *std.Io.Writer) !void {
     try writer.print(
@@ -26,6 +35,114 @@ fn openFile(io: std.Io, path: []const u8) !std.Io.File {
         ret = try std.Io.Dir.cwd().openFile(io, path, .{ .mode = .read_only });
     }
     return ret;
+}
+
+fn getFunctionName(tree: std.zig.Ast, nodeIndex: std.zig.Ast.Node.Index) []const u8 {
+    const node = tree.nodes.get(@intFromEnum(nodeIndex));
+    assert(node.tag == .fn_decl);
+    const start_token_index = tree.firstToken(node.data.node_and_node.@"0");
+    const end_token_index = tree.lastToken(node.data.node_and_node.@"0");
+    // use +1 as .. is not inclusive
+    for (start_token_index..end_token_index + 1) |index| {
+        const token = tree.tokens.get(index);
+        if (token.tag == .identifier) {
+            return tree.tokenSlice(@intCast(index));
+        }
+    }
+    unreachable;
+}
+
+fn spanToSlice(tree: std.zig.Ast, span: std.zig.Ast.Span) []const u8 {
+    return tree.source[span.start..span.end];
+}
+
+fn getFunctionReturnType(tree: std.zig.Ast, nodeIndex: std.zig.Ast.Node.Index) []const u8 {
+    const nodeData = tree.nodeData(nodeIndex);
+    const fnProtoNode = tree.nodes.get(@intFromEnum(nodeData.node_and_node.@"0"));
+    var returnIndex: std.zig.Ast.Node.Index = undefined;
+    switch (fnProtoNode.tag) {
+        .fn_proto_simple => {
+            returnIndex = fnProtoNode.data.opt_node_and_opt_node.@"1".unwrap().?;
+        },
+        .fn_proto_multi, .fn_proto_one, .fn_proto => {
+            returnIndex = fnProtoNode.data.extra_and_opt_node.@"1".unwrap().?;
+        },
+        else => {},
+    }
+    const startTokenIndex = tree.firstToken(returnIndex);
+    const lastTokenIndex = tree.lastToken(returnIndex);
+    return spanToSlice(tree, tree.tokensToSpan(startTokenIndex, lastTokenIndex, tree.nodeMainToken(returnIndex)));
+}
+
+fn getAllFunctionNames(tree: std.zig.Ast, gpa: std.mem.Allocator) ![][]const u8 {
+    var ret: std.ArrayList([]const u8) = .empty;
+    for (0..tree.nodes.len) |value| {
+        const node = tree.nodes.get(value);
+        if (node.tag == .fn_decl) {
+            const tmp = getFunctionName(tree, value);
+            try ret.append(gpa, tmp);
+        }
+    }
+    return ret.items;
+}
+
+fn getAllFunctions(tree: std.zig.Ast, gpa: std.mem.Allocator) ![]std.zig.Ast.Node.Index {
+    var ret: std.ArrayList(std.zig.Ast.Node.Index) = .empty;
+    for (tree.nodes.items(.tag), 0..) |value, index| {
+        if (value == .fn_decl) {
+            try ret.append(gpa, @enumFromInt(index));
+        }
+    }
+    return ret.items;
+}
+
+fn getFunctionArgsWithType(tree: std.zig.Ast, nodeIndex: std.zig.Ast.Node.Index, gpa: std.mem.Allocator) !std.AutoHashMap(u16, []const u8) {
+    var map: std.AutoHashMap(u16, []const u8) = .init(gpa);
+    var buffer: [1]std.zig.Ast.Node.Index = undefined;
+    const funcProto = tree.fullFnProto(&buffer, nodeIndex).?;
+    var iter = funcProto.iterate(&tree);
+    var index: u16 = 0;
+    while (iter.next()) |j| {
+        const startTokenIndex = tree.firstToken(j.type_expr.?);
+        const lastTokenIndex = tree.lastToken(j.type_expr.?);
+        try map.put(index, spanToSlice(tree, tree.tokensToSpan(startTokenIndex, lastTokenIndex, @intFromEnum(j.type_expr.?))));
+        index += 1;
+    }
+    return map;
+}
+
+fn getAllFunctionsWithAlloc(tree: std.zig.Ast, gpa: std.mem.Allocator) ![]std.zig.Ast.Node.Index {
+    const functionIndices = try getAllFunctions(tree, gpa);
+    var node: std.zig.Ast.Node = undefined;
+    var ret: std.ArrayList(std.zig.Ast.Node.Index) = .empty;
+    for (functionIndices) |i| {
+        node = tree.nodes.get(@intFromEnum(i));
+        var buffer: [1]std.zig.Ast.Node.Index = undefined;
+        const funcProto = tree.fullFnProto(&buffer, i).?;
+        var iter = funcProto.iterate(&tree);
+        while (iter.next()) |j| {
+            const startTokenIndex = tree.firstToken(j.type_expr.?);
+            const lastTokenIndex = tree.lastToken(j.type_expr.?);
+            if (std.mem.eql(u8, spanToSlice(tree, tree.tokensToSpan(startTokenIndex, lastTokenIndex, @intFromEnum(j.type_expr.?))), "std.mem.Allocator")) {
+                try ret.append(gpa, i);
+                break;
+            }
+        }
+    }
+    return ret.items;
+}
+
+fn getAllVariables(tree: std.zig.Ast, gpa: std.mem.Allocator) ![]std.zig.Ast.Node.Index {
+    var ret: std.ArrayList(std.zig.Ast.Node.Index) = .empty;
+    for (0..tree.nodes.len) |index| {
+        switch (tree.nodeTag(@enumFromInt(index))) {
+            .global_var_decl, .local_var_decl, .simple_var_decl, .aligned_var_decl => {
+                try ret.append(gpa, @enumFromInt(index));
+            },
+            else => {},
+        }
+    }
+    return ret.items;
 }
 
 pub fn main(init: std.process.Init) !void {
@@ -87,8 +204,13 @@ pub fn main(init: std.process.Init) !void {
     var ast = try std.zig.Ast.parse(gpa, src_code, .zig);
     defer ast.deinit(gpa);
 
-    for (ast.nodes.items(.tag)) |value| {
-        try stdout.print("{}\n", .{value});
+    for (try getAllFunctionsWithAlloc(ast, gpa)) |i| {
+        try stdout.print("{s}\n", .{getFunctionName(ast, i)});
+        const map = try getFunctionArgsWithType(ast, i, gpa);
+        var iter = map.keyIterator();
+        while (iter.next()) |value| {
+            try stdout.print("\t{d}: {s}\n", .{ value.*, map.get(value.*).? });
+        }
     }
     try stdout.flush();
 
---
+++ tests/hello_world.zig
@@ -0,0 +1,18 @@
+pub fn main() void {}
+pub fn foo() void {}
+pub fn bar() void {}
+pub fn baz() void {}
+pub fn qux() void {}
+pub fn quux() u8 {
+    qux();
+    qux();
+    qux();
+    qux();
+    qux();
+    qux();
+    qux();
+    qux();
+    qux();
+    qux();
+    qux();
+}
