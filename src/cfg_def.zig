f56098e04df032c9e44de33d51de5a01d5cee332
LindirQuenya <53021080+LindirQuenya@users.noreply.github.com>
2026-05-05 23:15:19 -0400

    Clear up typing definitions

--- src/cfg_def.zig
+++ src/cfg_def.zig
@@ -1,4 +1,46 @@
-const CFGNode = struct {
+// The first reference to a token within a scope is the canonical one.
+// Often, this is the variable definition or function prototype.
+pub const CanonicalToken = std.zig.Ast.TokenIndex;
+
+pub fn Set(comptime T: type) type {
+    return std.AutoHashMap(T, undefined);
+}
+
+pub const SourceState = union(enum) {
+    Uninit: undefined,
+    Alloc: CanonicalToken,
+    Maybe: CanonicalToken,
+};
+
+pub const SinkState = union(enum) {
+    Uninit: undefined,
+    Dealloc: CanonicalToken,
+    Maybe: CanonicalToken,
+};
+
+pub const MemoryOperation = union(enum) {
+    Allocation: struct {
+        allocator: CanonicalToken,
+        result: CanonicalToken,
+    },
+    FunctionCall: struct {
+        arguments: []CanonicalToken,
+        result: ?CanonicalToken,
+    },
+    Deallocation: struct {
+        allocator: CanonicalToken,
+        variable: CanonicalToken,
+    },
+    Deinit: struct {
+        variable: CanonicalToken,
+    },
+    DeinitExplicit: struct {
+        allocator: CanonicalToken,
+        variable: CanonicalToken,
+    },
+};
+
+pub const CFGNode = struct {
     nodes_in: []*CFGNode,
     nodes_out: []*CFGNode,
     type: enum {
@@ -10,8 +52,10 @@ const CFGNode = struct {
     },
     // The token is the "canonical" token for a given variable -- the declaration, for example.
     // If a token doesn't have an entry, it is implicitly Uninit.
-    sources_out: std.AutoHashMap(std.zig.Ast.TokenIndex, Set(SourceState)),
-    sinks_out: std.AutoHashMap(std.zig.Ast.TokenIndex, Set(SinkState)),
+    sources_out: std.AutoHashMap(CanonicalToken, Set(SourceState)),
+    sinks_out: std.AutoHashMap(CanonicalToken, Set(SinkState)),
     ast_nodes: []std.zig.Ast.Node.Index,
+    // At most one memory operation per node.
+    mem_op: ?MemoryOperation,
 };
 const std = @import("std");
